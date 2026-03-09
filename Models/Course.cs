c2545682143f0687bd6ac93b693e2ece87d0b258
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2026-03-09 04:05:12 +0300

    Add Assignment 2 EF Core university project

---
+++ Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityCourseManagement.Models
+{
+    public class Course
+    { public int CourseId { get; set; }
+        public string CourseName { get; set; } = null!;
+         public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+          
+        public int TeacherId { get; set; }
+         public User Teacher { get; set; } = null!;
+        public ICollection<Assignment> Assignments { get; set; } = [];
+        public Syllabus Syllabus { get; set; } = null!;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityCourseManagement.Models
+{
+    public class User
+    {   public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string EmailAddress { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Role { get; set; } = null!;
+
+        public ICollection<Course> TaughtCourses { get; set; } = [];
+        public ICollection<Comment> Comments { get; set; } = [];
+        public ICollection<Grade> Grades { get; set; } = [];
+            
+
+
+
+        
+    }
+}
